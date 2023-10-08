58ec2b02bcba9a3aceb0a4da7a65016a38df01f6
gowthamEntrans <gowtham@entrans.io>
2023-10-08 23:59:56 +0530

    initial commit

--- index.js
+++ index.js
@@ -2,13 +2,21 @@ const express = require("express");
 const app = express();
 require("dotenv").config;
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/',(req,res)=>{
-    res.send("hello world")
-})
-
-
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+app.post("/none", (req, res) => {
+  const { helo } = req.body;
+  if (helo === 1) {
+    console.log("success");
+    res.send("sucess");
+  } else {
+    console.log("again");
+    res.send("again");
+  }
+});
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
