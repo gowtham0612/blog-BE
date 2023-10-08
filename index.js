35e33ed306e736ce1433d6038fc3f78654babeba
gowthamEntrans <gowtham@entrans.io>
2023-10-08 23:48:30 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+require("dotenv").config;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+const port = process.env.PORT || 4000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
