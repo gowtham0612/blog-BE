7067c4b218170065cbb4e079abb6f4ecc2169a7b
gowthamEntrans <gowtham@entrans.io>
2023-10-08 23:55:00 +0530

    initial commit

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const express = require("express");
 const app = express();
 require("dotenv").config;
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }))
 
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
+app.get('/',(req,res)=>{
+    res.send("hello world")
+})
+
 
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
